c46e91fc68b77b601b2d3e098accb9f67a9b45a9
Ram <harsha@new-host.home>
2012-01-15 18:59:36 -0500

    Iterate code + test case

--- src/test/java/org/terpworks/yahoo/IterateTest.java
+++ src/test/java/org/terpworks/yahoo/IterateTest.java
@@ -1,14 +1,46 @@
 package org.terpworks.yahoo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Iterator;
 
 import org.junit.Test;
+import org.terpworks.yahoo.Iterate.Function;
 
 public class IterateTest {
 
 	@Test
 	public void testIterate() {
-		fail("Not yet implemented");
+		
+		Iterator<Integer> results = Iterate.iterate(new Function<Integer>(){
+
+			public Integer apply(Integer argument) {
+				// TODO Auto-generated method stub
+				return argument*argument;
+			}
+			
+		}, 3);
+		
+		assertEquals(results.next(),(Integer) 3);
+		assertEquals(results.next(),(Integer) 9);
+		assertEquals(results.next(),(Integer)81);
+		
+		results.remove();
+		assertEquals(results.next(),(Integer)81);
+		
+		results.remove();
+		Exception t = null;
+		try{ 
+			results.remove();
+		}catch(Exception e) {
+			t = e;
+		}
+		
+		assertNotNull(t);
+		assertTrue(t instanceof IllegalStateException);
 	}
 
+	
 }
