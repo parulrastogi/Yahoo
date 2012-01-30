2aa23590150f559b66c4f9487fd1b209d4400a14
Ram <harsha@harsha.home>
2012-01-29 20:35:17 -0500

    Disjoint Set Data Structure, skeleton code

--- src/main/java/org/terpworks/yahoo/DefaultDisjointSet.java
+++ src/main/java/org/terpworks/yahoo/DefaultDisjointSet.java
@@ -1,6 +1,7 @@
 package org.terpworks.yahoo;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Disjoint Set, represented as an array
@@ -10,20 +11,22 @@ import java.util.ArrayList;
  */
 public class DefaultDisjointSet<T> implements DisjointSet<T> {
 
-	private ArrayList<T> elements = new ArrayList<T>();
+	private Map<T, Integer> index = new HashMap<T, Integer>();
+	private Map<Integer, T> invertedIndex = new HashMap<Integer, T>();
 	
 	private int[]  delegate;
 	
 	
 	public DefaultDisjointSet(T[] elements) {
 		
-		for(T element : elements) {
-			
-			this.elements.add(element);
-		}
+		
 		delegate = new int[elements.length];
-		for(int i=0;i<elements.length;i++)
+		for(int i=0;i<elements.length;i++){
+			
 			delegate[i]=-1;
+			index.put(elements[i], i);
+			invertedIndex.put(i,elements[i]);
+		}
 		
 	}
 	public void createSet(T element) {
@@ -32,12 +35,40 @@ public class DefaultDisjointSet<T> implements DisjointSet<T> {
 	}
 
 	public T findSet(T element) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if(!index.containsKey(element)) return null;
+		int arrayIndex = index.get(element);
+		int originalArrayIndex = arrayIndex;
+		
+		int originalParentIndex = delegate[arrayIndex];
+		int parentIndex = originalParentIndex;
+
+		while(parentIndex != -1) {
+			arrayIndex = parentIndex;
+			parentIndex = delegate[arrayIndex];
+		}
+		
+		if(originalParentIndex != -1)
+			delegate[originalArrayIndex] = arrayIndex;
+		return invertedIndex.get(arrayIndex) ;
 	}
 
-	public void mergeSet(T parent, T child) {
+	public void  mergeSet(T representativeOfFirstSet, T representativeOfSecondSet) {
 		// TODO Auto-generated method stub
+		if(!index.containsKey(representativeOfFirstSet) 
+				|| !index.containsKey(representativeOfSecondSet))
+			return;
+		
+		int firstIndex = index.get(findSet(representativeOfFirstSet));
+		int secondIndex = index.get(findSet(representativeOfSecondSet));
+		
+		if(firstIndex <  secondIndex) {
+			
+			delegate[secondIndex] = firstIndex;
+		} else {
+			
+			delegate[firstIndex] = secondIndex;
+		}
 		
 	}
 
